649e72d1ea353f3c92486fc6160c2bf9dbd6ceb9
Arnau Quera-Bofarull <arnau.quera-bofarull@durham.ac.uk>
2020-06-13 17:22:42 +0200

    RE tau_x

--- src/Radiation.jl
+++ src/Radiation.jl
@@ -1,7 +1,11 @@
 using Interpolations, Roots
-export xray_opacity,
-    xray_tau, uv_tau, uv_opacity, ionization_parameter, force_multiplier
-export NoInterp, Interp, QuadTree, NoGrid
+export compute_xray_opacity,
+    compute_xray_tau,
+    compute_uv_tau,
+    compute_uv_opacity,
+    compute_ionization_parameter,
+    compute_force_multiplier
+export NoInterp, Interp, QuadTree, NoGrid, RE
 abstract type Flag end
 abstract type GridType <: Flag end
 abstract type InterpolationType <: Flag end
@@ -9,13 +13,21 @@ struct NoInterp <: InterpolationType end
 struct Interp <: InterpolationType end
 struct QuadTree <: GridType end
 struct NoGrid <: GridType end
+abstract type RadiationMode <: Flag end
+struct RE <: RadiationMode end
 
 ## X-Ray phyiscs
 
 """
 Ionization parameter ξ
 """
-function ionization_parameter(r, z, number_density, tau_x, xray_luminosity)
+function compute_ionization_parameter(
+    r,
+    z,
+    number_density,
+    tau_x,
+    xray_luminosity,
+)
     d = sqrt(r^2 + z^2)
     return xray_luminosity * exp(-tau_x) / (number_density * d^2)
 end
@@ -23,7 +35,7 @@ end
 """
 X-Ray opacity as a function of ionization parameter.
 """
-function xray_opacity(ionization_parameter)
+function compute_xray_opacity(ionization_parameter)
     if ionization_parameter < 1e5
         return 100 * SIGMA_T
     else
@@ -31,19 +43,14 @@ function xray_opacity(ionization_parameter)
     end
 end
 
-"""
-If we want to solve the equation ξ(r_x) = ξ_0, then we need to solve
-the implicit equation ξ_0 = L_x / (n r_x^2) * exp(-σ * n * (r_x - r_in)),
-where we introduce the change t = log(r_x) to make it less suitable to roundoff
-errors.
+function compute_xray_opacity(r, r_x)
+    if r > r_x
+        return 100.0 * SIGMA_T
+    else
+        return SIGMA_T
+    end
+end
 
-# Parameters
-- xray_luminosity (in cgs)
-- number_density (in cgs)
-- r_in initial material radius (in cgs)
-- r_x ionization radius candidate (in cgs)
-- target ionization parameter (in cgs)
-"""
 function ionization_radius_kernel(
     xray_luminosity,
     number_density,
@@ -57,7 +64,22 @@ function ionization_radius_kernel(
     ) - number_density * SIGMA_T * (r_x - r_in)
 end
 
-function ionization_radius(
+"""
+If we want to solve the equation ξ(r_x) = ξ_0, then we need to solve
+the implicit equation ξ_0 = L_x / (n r_x^2) * exp(-σ * n * (r_x - r_in)),
+where we introduce the change t = log(r_x) to make it less suitable to roundoff
+errors.
+
+# Parameters
+- xray_luminosity (in cgs)
+- number_density (in cgs)
+- r_in initial material radius (in cgs)
+- target ionization parameter (in cgs)
+
+# Returns
+- r_x ionization radius (in cgs)
+"""
+function compute_ionization_radius(
     xray_luminosity,
     number_density,
     r_in,
@@ -87,7 +109,7 @@ X-Ray optical depth
 - ionization_parameter: (in cgs)
 
 """
-function xray_tau(
+function compute_xray_tau(
     r,
     z,
     number_density,
@@ -96,10 +118,10 @@ function xray_tau(
     z_1 = 0.0,
 )
     d = sqrt((r - r_1)^2 + (z - z_1)^2)
-    return number_density * xray_opacity(ionization_parameter) * d
+    return number_density * compute_xray_opacity(ionization_parameter) * d
 end
 
-function xray_tau(
+function compute_xray_tau(
     r::Vector,
     z::Vector,
     number_density::Vector,
@@ -108,7 +130,7 @@ function xray_tau(
     ret = 0.0
     r1, z1 = 0.0, 0.0
     for i = 1:length(r)
-        ret += xray_tau(
+        ret += compute_xray_tau(
             r[i],
             z[i],
             number_density[i],
@@ -121,6 +143,62 @@ function xray_tau(
     return ret
 end
 
+"""
+Risaliti & Elvis (2010) implementation of the X-Ray optical depth.
+
+# Parameters
+- r : radial position (cm)
+- z : height position (cm)
+- shielding density : average atmospheric number density (cm^-3)
+- local density : local density at the position (cm^-3)
+- r_in : initial radius of the material(wind) (cm)
+- r_x : ionization radius (cm)
+- r_0 : initial radius of the current streamline (cm)
+- mode : RE mode
+"""
+function compute_xray_tau(
+    r,
+    z,
+    shielding_density,
+    local_density,
+    r_in,
+    r_x,
+    r_0;
+    mode::RE,
+)
+    if r <= r_in
+        return 0.0
+    end
+    if r <= r_0
+        tau_r = 0.0
+        if r_x < r
+            tau_r_0 = shielding_density * ((r_x - r_in) + (r - r_x) * 100)
+        else
+            tau_r_0 = shielding_density * (r - r_in)
+        end
+    else
+        if r_x < r
+            if r_0 < r_x
+                tau_r_0 = shielding_density * (r_0 - r_in)
+                if r < r_x
+                    tau_r = local_density * (r - r_x)
+                else
+                    tau_r = local_density * ((r_x - r_0) + (r-r_x) * 100)
+                end
+            else
+                tau_r_0 = shielding_density * ((r_x - r_in) + 100 * (r_0 - r_x))
+                tau_r = local_density * (r-r_0) * 100
+            end
+        else
+            tau_r_0 = shielding_density * (r_0 - r_in)
+            tau_r = local_density * (r - r_0)
+        end
+    end
+    upper_projection = 1.0 / cos(r / sqrt(r^2 + z^2))
+    return (tau_r + tau_r_0) * SIGMA_T * upper_projection
+end
+
+
 ## UV Physics
 
 
@@ -129,9 +207,9 @@ UV opacity
 """
 uv_opacity() = SIGMA_T
 
-function uv_tau(r, z, number_density, r_0 = 0.0, z_0 = 0.0)
+function compute_uv_tau(r, z, number_density, r_0 = 0.0, z_0 = 0.0)
     d = sqrt((r - r_0)^2 + (z - z_0)^2)
-    return number_density * uv_opacity() * d
+    return number_density * compute_uv_opacity() * d
 end
 
 
@@ -168,12 +246,12 @@ force_multiplier_eta_log_interpolator = extrapolate(
     ),
     Flat(),
 )
-force_multiplier_eta(ionization_parameter, mode::Interp) =
+compute_force_multiplier_eta(ionization_parameter, mode::Interp) =
     10 .^ force_multiplier_eta_log_interpolator(log10(ionization_parameter))
-force_multiplier_eta(ionization_parameter) =
-    force_multiplier_eta(ionization_parameter, Interp())
+compute_force_multiplier_eta(ionization_parameter) =
+    compute_force_multiplier_eta(ionization_parameter, Interp())
 
-function force_multiplier_eta(ionization_parameter, mode::NoInterp)
+function compute_force_multiplier_eta(ionization_parameter, mode::NoInterp)
     if (log10(ionization_parameter) < 0.5)
         aux = 6.9 * exp(0.16 * ionization_parameter^0.4)
         eta_max = 10^aux
@@ -188,13 +266,17 @@ Computes the analytical approximation for the force multiplier,
 from Stevens and Kallman 1990. Note that we modify it slightly to avoid
 numerical overflow.
 """
-function force_multiplier(t, ionization_parameter, mode::InterpolationType)
+function compute_force_multiplier(
+    t,
+    ionization_parameter,
+    mode::InterpolationType,
+)
     @assert t >= 0
     @assert ionization_parameter >= 0
     ALPHA = 0.6
     TAU_MAX_TOL = 1e-3
-    k = force_multiplier_k(ionization_parameter, mode)
-    eta = force_multiplier_eta(ionization_parameter, mode)
+    k = compute_force_multiplier_k(ionization_parameter, mode)
+    eta = compute_force_multiplier_eta(ionization_parameter, mode)
     tau_max = t * eta
     if tau_max < TAU_MAX_TOL
         aux = (1 - ALPHA) * (tau_max^ALPHA)
@@ -206,5 +288,5 @@ function force_multiplier(t, ionization_parameter, mode::InterpolationType)
     return fm
 end
 
-force_multiplier(t, ionization_parameter) =
-    force_multiplier(t, ionization_parameter, Interp())
+compute_force_multiplier(t, ionization_parameter) =
+    compute_force_multiplier(t, ionization_parameter, Interp())
--- test/radiations_tests.jl
+++ test/radiations_tests.jl
@@ -3,25 +3,51 @@ using Test
 
 
 @testset "Ionization Parameter" begin
-    @test ionization_parameter(1, 1, 10, 0, 1) ≈ 1 / 20
-    @test ionization_parameter(1, 1, 10, 100, 1) ≈ 0 atol = 1e-20
-    @test ionization_parameter(1, 1, 10, 0, 2) ≈ 1 / 10
+    @test compute_ionization_parameter(1, 1, 10, 0, 1) ≈ 1 / 20
+    @test compute_ionization_parameter(1, 1, 10, 100, 1) ≈ 0 atol = 1e-20
+    @test compute_ionization_parameter(1, 1, 10, 0, 2) ≈ 1 / 10
 end
 
 @testset "X-Ray Opacity" begin
-    @test xray_opacity(1) == 100 * SIGMA_T
-    @test xray_opacity(9e4) == 100 * SIGMA_T
-    @test xray_opacity(1e5) == SIGMA_T
-    @test xray_opacity(1e6) == SIGMA_T
+    @test compute_xray_opacity(1) == 100 * SIGMA_T
+    @test compute_xray_opacity(9e4) == 100 * SIGMA_T
+    @test compute_xray_opacity(1e5) == SIGMA_T
+    @test compute_xray_opacity(1e6) == SIGMA_T
 end
 
 @testset "X-Ray optical depth" begin
-    @test xray_tau(3, 4, 2, 1e6) == 10 * SIGMA_T
-    @test xray_tau(3, 4, 2, 1e-2) ≈ 1000 * SIGMA_T
-    @test xray_tau(3, 4, 4, 1e6) == 20 * SIGMA_T
-    @test xray_tau(12, 16, 2, 1e-2) ≈ 4000 * SIGMA_T
-    @test xray_tau(4, 5, 2, 1e6, 1, 1) == 10 * SIGMA_T
-    @test xray_tau([1, 3], [4 / 3, 4], [2, 2], [1e6, 1e6]) == 10 * SIGMA_T
+    @test compute_xray_tau(3, 4, 2, 1e6) == 10 * SIGMA_T
+    @test compute_xray_tau(3, 4, 2, 1e-2) ≈ 1000 * SIGMA_T
+    @test compute_xray_tau(3, 4, 4, 1e6) == 20 * SIGMA_T
+    @test compute_xray_tau(12, 16, 2, 1e-2) ≈ 4000 * SIGMA_T
+    @test compute_xray_tau(4, 5, 2, 1e6, 1, 1) == 10 * SIGMA_T
+    @test compute_xray_tau([1, 3], [4 / 3, 4], [2, 2], [1e6, 1e6]) ==
+          10 * SIGMA_T
+end
+
+@testset "RE X-ray optical depth" begin
+    # r<= r_in
+    @test compute_xray_tau(4, 3, 10, 20, 5, 100, 2, mode = RE()) / SIGMA_T ==
+          0.0
+    # r < r_0, r_x < r
+    @test compute_xray_tau(4, 3, 10, 20, 1, 2, 6, mode = RE()) / SIGMA_T ≈
+          2010 / cos(4 / 5)
+    # r < r_0, r_x > r
+    @test compute_xray_tau(4, 3, 10, 20, 1, 5, 6, mode = RE()) / SIGMA_T ≈
+          30 / cos(4 / 5)
+    # r > r_0, r_in < r_x < r_0
+    @test compute_xray_tau(4, 3, 10, 20, 1, 2, 3, mode = RE()) / SIGMA_T ≈
+          3010 / cos(4 / 5)
+    # r > r_0, r_in < r_x < r_0
+    @test compute_xray_tau(4, 3, 10, 20, 1, 3.5, 3, mode = RE()) / SIGMA_T ≈
+          1030 / cos(4 / 5)
+    # r > r_0, r_in < r_0 < r < r
+    @test compute_xray_tau(4, 3, 10, 20, 1, 5, 2, mode = RE()) / SIGMA_T ≈
+          50 / cos(4 / 5)
+    # r > r_0, r_in < r_0 < r_x < r
+    @test compute_xray_tau(4, 3, 10, 20, 1, 3, 2, mode = RE()) / SIGMA_T ≈
+          2030 / cos(4 / 5)
+
 end
 
 @testset "Ionization radius" begin
@@ -29,41 +55,46 @@ end
     density = 10.0
     r_in = 1.0
     xi_0 = 1e5
-    r_x = Qwind.ionization_radius(xray_lumin, density, r_in, atol = 1e-2)
+    r_x =
+        Qwind.compute_ionization_radius(xray_lumin, density, r_in, atol = 1e-2)
     @test Qwind.ionization_radius_kernel(xray_lumin, density, r_in, r_x, xi_0) ≈
           0 atol = 1e-2
-    tau_x = xray_tau(r_x, 0, density, xi_0)
-    @test ionization_parameter(r_x, 0, density, tau_x, xray_lumin) ≈ xi_0
-    r_x = Qwind.ionization_radius(xray_lumin, density, r_in, atol = 1e-4)
+    tau_x = compute_xray_tau(r_x, 0, density, xi_0)
+    @test compute_ionization_parameter(r_x, 0, density, tau_x, xray_lumin) ≈
+          xi_0
+    r_x =
+        Qwind.compute_ionization_radius(xray_lumin, density, r_in, atol = 1e-4)
     @test Qwind.ionization_radius_kernel(xray_lumin, density, r_in, r_x, xi_0) ≈
           0 atol = 1e-4
-    tau_x = xray_tau(r_x, 0, density, xi_0)
-    @test ionization_parameter(r_x, 0, density, tau_x, xray_lumin) ≈ xi_0
+    tau_x = compute_xray_tau(r_x, 0, density, xi_0)
+    @test compute_ionization_parameter(r_x, 0, density, tau_x, xray_lumin) ≈
+          xi_0
 end
 
 @testset "UV optical depth" begin
-    @test uv_tau(3, 4, 2) == 10 * SIGMA_T
-    @test uv_tau(3, 4, 4) == 20 * SIGMA_T
-    @test uv_tau(12, 16, 2) ≈ 40 * SIGMA_T
-    @test uv_tau(4, 5, 2, 1, 1) == 10 * SIGMA_T
+    @test compute_uv_tau(3, 4, 2) == 10 * SIGMA_T
+    @test compute_uv_tau(3, 4, 4) == 20 * SIGMA_T
+    @test compute_uv_tau(12, 16, 2) ≈ 40 * SIGMA_T
+    @test compute_uv_tau(4, 5, 2, 1, 1) == 10 * SIGMA_T
 end
 
 @testset "Force multiplier" begin
-    @test Qwind.force_multiplier_k(1e-3) == 0.411
-    @test Qwind.force_multiplier_k(10^3.51) == 0.045
-    @test Qwind.force_multiplier_k(1e-5) == 0.411
-    @test Qwind.force_multiplier_k(1e10) == 0.013
-    @test Qwind.force_multiplier_eta(1e-3) == 10^6.95
-    @test Qwind.force_multiplier_eta(10^3.50) == 10^1.58
-    @test Qwind.force_multiplier_eta(1e-5) == 10^6.95
-    @test Qwind.force_multiplier_eta(1e10) == 10^0.78
-    @test force_multiplier(1e5, 1e5) < 1e-3
-    @test force_multiplier(1e5, 1e-5) < 1e-3
-    @test force_multiplier(1e-8, 1e-5) > 2000
-    @test force_multiplier(1e-8, 1e-5) < 3000
-    @test force_multiplier(1e-10, 1e-5) ≈ 2300 atol = 0 rtol = 0.1
-    @test force_multiplier(1e-2, 1e-5) ≈ 6 atol = 0 rtol = 0.1
+    @test Qwind.compute_force_multiplier_k(1e-3) == 0.411
+    @test Qwind.compute_force_multiplier_k(10^3.51) == 0.045
+    @test Qwind.compute_force_multiplier_k(1e-5) == 0.411
+    @test Qwind.compute_force_multiplier_k(1e10) == 0.013
+    @test Qwind.compute_force_multiplier_eta(1e-3) == 10^6.95
+    @test Qwind.compute_force_multiplier_eta(10^3.50) == 10^1.58
+    @test Qwind.compute_force_multiplier_eta(1e-5) == 10^6.95
+    @test Qwind.compute_force_multiplier_eta(1e10) == 10^0.78
+    @test compute_force_multiplier(1e5, 1e5) < 1e-3
+    @test compute_force_multiplier(1e5, 1e-5) < 1e-3
+    @test compute_force_multiplier(1e-8, 1e-5) > 2000
+    @test compute_force_multiplier(1e-8, 1e-5) < 3000
+    @test compute_force_multiplier(1e-10, 1e-5) ≈ 2300 atol = 0 rtol = 0.1
+    @test compute_force_multiplier(1e-2, 1e-5) ≈ 6 atol = 0 rtol = 0.1
 end
+
 
 #@testset "Force radiation" begin
 #    @test all(isapprox.(
