237b69afe28ab9edb194a37f4a8e686065f7e555
Arnau Quera-Bofarull <arnau.quera-bofarull@durham.ac.uk>
2020-12-22 21:08:10 +0100

    new travis setup

--- src/radiation/qsosed.jl
+++ src/radiation/qsosed.jl
@@ -1,5 +1,6 @@
 using RegionTrees, PyCall
-export ConstantFUV, QsosedRadiation, from_quadtree, getfuv, compute_disk_temperature, getmdot, getridx
+export ConstantFUV,
+    QsosedRadiation, from_quadtree, getfuv, compute_disk_temperature, getmdot, getridx
 struct ConstantFUV <: Flag end
 struct ConstantMdot <: Flag end
 
@@ -17,20 +18,36 @@ end
 
 function QsosedRadiation(bh::BlackHole, nr::Int, fx::Float64)
     qsosed_python = pyimport("qsosed")
-    sed = qsosed_python.SED(M=bh.M / M_SUN, mdot=bh.mdot, number_bins_fractions=nr)
+    sed = qsosed_python.SED(M = bh.M / M_SUN, mdot = bh.mdot, number_bins_fractions = nr)
     rmin = bh.isco
     rmax = 1400
-    disk_grid, uv_fractions = sed.compute_uv_fractions(inner_radius=rmin, outer_radius=rmax, return_all=false, log_spaced=true)
+    disk_grid, uv_fractions = sed.compute_uv_fractions(
+        inner_radius = rmin,
+        outer_radius = rmax,
+        return_all = false,
+        log_spaced = true,
+    )
     mdot_grid = bh.mdot .* ones(length(disk_grid))
     xray_luminosity = fx * compute_bolometric_luminosity(bh)
-    return QsosedRadiation(disk_grid, uv_fractions, mdot_grid, xray_luminosity, sed, bh.efficiency, bh.spin, bh.isco, bh.Rg)
+    return QsosedRadiation(
+        disk_grid,
+        uv_fractions,
+        mdot_grid,
+        xray_luminosity,
+        sed,
+        bh.efficiency,
+        bh.spin,
+        bh.isco,
+        bh.Rg,
+    )
 end
 
 getridx(radiation::QsosedRadiation, r) = searchsortednearest(radiation.disk_grid, r)
 getfuv(radiation::QsosedRadiation, flag::ConstantFUV) = radiation.fuv_grid[1]
-getfuv(radiation::QsosedRadiation, r) = radiation.fuv_grid[getridx(radiation, r)]  
+getfuv(radiation::QsosedRadiation, r) = radiation.fuv_grid[getridx(radiation, r)]
 getmdot(radiation::QsosedRadiation, flag::ConstantMdot) = radiation.mdot_grid[1]
-getmdot(radiation::QsosedRadiation, r) = radiation.mdot_grid[getridx(radiation, r)]  
+getmdot(radiation::QsosedRadiation, r) = radiation.mdot_grid[getridx(radiation, r)]
 compute_radiation_constant(radiation::QsosedRadiation) = 6 / (8 * π * radiation.efficiency)
 
-compute_disk_temperature(radiation::QsosedRadiation, r) = radiation.sed.disk_nt_temperature4(r)^(1.0 / 4.0)
+compute_disk_temperature(radiation::QsosedRadiation, r) =
+    radiation.sed.disk_nt_temperature4(r)^(1.0 / 4.0)
