c4fcb742273cfd844dccbc6cb6c0fe8e82684ec8
Arnau Quera-Bofarull <arnau.quera-bofarull@durha.ac.uk>
2021-01-15 14:34:58 +0000

    Implemented detailed BB physics

--- src/radiation/disk.jl
+++ src/radiation/disk.jl
@@ -1,15 +1,133 @@
-export BlackBody, spectral_radiance
+using QuadGK
+export BlackBody,
+    spectral_radiance,
+    spectral_radiance_frequency,
+    spectral_band_radiance,
+    spectral_band_radiance_frequency,
+    spectral_band_fraction,
+    spectral_band_fraction_frequency,
+    radiance,
+    disk_nt_rel_factors,
+    disk_flux,
+    disk_temperature,
+    uv_fraction
 
 struct BlackBody
     T::Float64
 end
 
+kev_to_hz(energy) = energy / ERG_TO_KEV / H_PLANCK
+
+"""
+Spectral radiance of a black body bb for a frequency. Input frequency is
+in Hz, and output units are erg / (s cm^2 sr Hz)
+"""
+function spectral_radiance_frequency(bb::BlackBody, frequency)
+    a = 2 * H_PLANCK * frequency^3 / C^2
+    b = 1 / (exp((H_PLANCK * frequency) / (K_B * bb.T)) - 1)
+    return a * b
+end
+
+"""
+Spectral radiance of a black body at a given energy. The input energy
+has units of keV, and the result output is in units of
+keV / (s cm² keV sr)
+"""
 function spectral_radiance(bb::BlackBody, energy)
-    a = 2 / (H_PLANCK^2 * C^2)
-    # energy to ergs
-    energy_erg = energy / ERG_TO_KEV
-    b = energy_erg^3 / (exp(energy_erg / (K_B * bb.T)) - 1)
-    ret = a * b
-    # convert back to keV
-    ret = ret * ERG_TO_KEV / HZ_TO_KEV
+    frequency = kev_to_hz(energy)
+    rad = spectral_radiance_frequency(bb, frequency)
+    return rad 
+end
+
+"""
+Computes the radiance on a frequency band from low to high.
+"""
+function spectral_band_radiance_frequency(bb::BlackBody, low, high)
+    integral, err = quadgk(x -> spectral_radiance_frequency(bb, x), low, high, rtol = 1e-8)
+    return integral
+end
+
+"""
+Computes the radiance on an energetic band from low to high.
+"""
+function spectral_band_radiance(bb::BlackBody, low, high)
+    low = kev_to_hz(low)
+    high = kev_to_hz(high)
+    integral, err = quadgk(x -> spectral_radiance_frequency(bb, x), low, high, rtol = 1e-8)
+    return integral
+end
+
+radiance(bb::BlackBody) = SIGMA_SB * bb.T^4 / π 
+
+"""
+Computes the amount of black body radiance that is emitted in
+a particular frequency band.
+"""
+function spectral_band_fraction_frequency(bb::BlackBody, low, high)
+    total_radiance = radiance(bb)
+    band_radiance = spectral_band_radiance_frequency(bb, low, high)
+    return band_radiance / total_radiance
+end
+"""
+Computes the amount of black body radiance that is emitted in
+a particular energy band.
+"""
+function spectral_band_fraction(bb::BlackBody, low, high)
+    total_radiance = radiance(bb)
+    band_radiance = spectral_band_radiance(bb, low, high)
+    return band_radiance / total_radiance
+end
+
+"""
+Novikov-Thorne relativistic factors for the AD spectrum.
+If the passed radius is smaller than ISCO, it returns 0.
+
+Parameters
+----------
+radius
+    disc radius measured in R_g
+spin
+    normalized black hole spin (-1, 1)
+isco
+    innermost stable circular orbit measured in Rg
+"""
+function disk_nt_rel_factors(radius, spin, isco)
+    if radius <= isco
+        return 0.0
+    end
+    yms = sqrt(isco)
+    y1 = 2 * cos((acos(spin) - pi) / 3)
+    y2 = 2 * cos((acos(spin) + pi) / 3)
+    y3 = -2 * cos(acos(spin) / 3)
+    y = sqrt(radius)
+    C = 1 - 3 / radius + 2 * spin / radius^1.5
+    B = 3 * (y1 - spin)^2 * log((y - y1) / (yms - y1)) / (y * y1 * (y1 - y2) * (y1 - y3))
+    B += 3 * (y2 - spin)^2 * log((y - y2) / (yms - y2)) / (y * y2 * (y2 - y1) * (y2 - y3))
+    B += 3 * (y3 - spin)^2 * log((y - y3) / (yms - y3)) / (y * y3 * (y3 - y1) * (y3 - y2))
+    A = 1 - yms / y - 3 * spin * log(y / yms) / (2 * y)
+    factor = (A - B) / C
+    return factor
+end
+
+disk_nt_rel_factors(bh::BlackHole, r) = disk_nt_rel_factors(r, bh.spin, bh.isco)
+
+"""
+Computes the disk flux of the accretion disc of black hole ``bh``
+at a given radius ``r`` (in Rg). See equation 5 of https://arxiv.org/pdf/2001.04720.pdf
+"""
+function disk_flux(bh::BlackHole, r)
+    Mdot = compute_mass_accretion_rate(bh)
+    NT_factors = disk_nt_rel_factors(bh, r)
+    return 3 * G * bh.M * Mdot / (8 * π * (r * bh.Rg)^3) * NT_factors
+end
+
+function disk_temperature(bh::BlackHole, r)
+    flux = disk_flux(bh, r)
+    return (flux / SIGMA_SB)^(1/4)
+end
+
+function uv_fraction(bh::BlackHole, r)
+    temperature = disk_temperature(bh, r)
+    bb = BlackBody(temperature)
+    return spectral_band_fraction(bb, UV_LOW_KEV, UV_HIGH_KEV)
 end
--- test/radiation/disk_tests.jl
+++ test/radiation/disk_tests.jl
@@ -1,32 +1,59 @@
 using Qwind
 using Test
 
-@testset "Blackbody radiation" begin
-    @testset "Test spectral radiance" begin
-        # Tested with the Astropy BlackBody model.
-        bb = BlackBody(1e5)
-        @test spectral_radiance(bb, 0.1) ≈ 2.8706e23 / H_PLANCK rtol = 1e-3
-        @test spectral_radiance(bb, 0.02) ≈ 2.74e25 / H_PLANCK rtol = 1e-2
-        bb = BlackBody(5e3)
-        @test spectral_radiance(bb, 0.001) ≈ 3.425e21 / H_PLANCK rtol = 1e-3
-    end
-    @testset "Test radiance" begin
-    end
-end
-
 @testset "Disk radiation" begin
-    bh = BlackHole(1e8, 0.5, 0)
-    radiation = QsosedRadiation(bh, 1000, 0.15)
-    @testset "Disk flux" begin
-        @test compute_disk_temperature(radiation, 100)^4 ≈ 6.920770795914223e+17 rtol = 1e-5
-        @test compute_disk_temperature(radiation, 10)^4 ≈ 1.2061136229423976e+20 rtol = 1e-5
+    @testset "Blackbody radiation" begin
+        @testset "Test spectral radiance" begin
+            bb = BlackBody(1e5)
+            @test spectral_radiance_frequency(bb, 100) ≈ 3.07235837e-28 rtol = 1e-3
+            @test spectral_radiance_frequency(bb, 1e15) ≈ 0.02393854 rtol = 1e-4
+            @test spectral_radiance_frequency(bb, 1e18) ≈ 0. rtol = 1e-6 atol = 1e-15
+            bb = BlackBody(5e3)
+            @test spectral_radiance_frequency(bb, 100) ≈ 1.53617919e-29 rtol = 1e-4
+            @test spectral_radiance_frequency(bb, 1e15) ≈ 1.00024067e-06
+            @test spectral_radiance_frequency(bb, 1e18) ≈ 0
+        end
+        @testset "Test integrating energy band" begin
+            bb = BlackBody(1e4)
+            @test spectral_band_radiance_frequency(bb, 100e12, 1000e12) ≈ 1.30413e11 rtol = 1e-2
+            @test spectral_band_radiance(bb, 0.00041357, 0.0041357) ≈ 1.30413e11 rtol = 1e-2
+        end
+        @testset "Integrating spectral radiance recovers SB law" begin
+            bb = BlackBody(1e4)
+            @test spectral_band_radiance(bb, 1e-5, 1e2) ≈ SIGMA_SB * bb.T^4 / π rtol = 1e-6
+        end
+        @testset "Energy band fraction" begin
+            bb = BlackBody(1e4)
+            @test spectral_band_fraction_frequency(bb, 1e12, 1e18) ≈ 1 rtol = 1e-8 
+            @test spectral_band_fraction(bb, 1e-5, 1e2) ≈ 1 rtol = 1e-6
+        end
     end
-
-    @testset "BB spectral radiance" begin
-        @test compute_bb_spectral_radiance()
+    @testset "Disk radiation" begin
+        @testset "Test relativistic AD correction" begin
+            bh = BlackHole(1e8, 0.5, 0.6)
+            @test disk_nt_rel_factors(bh, 10) ≈ 0.2615157665830733
+            @test disk_nt_rel_factors(bh, 50) ≈ 0.6086578218842061
+            @test disk_nt_rel_factors(bh, 200) ≈ 0.7849369908504347
+            bh = BlackHole(1e9, 0.5, 0.99)
+            @test disk_nt_rel_factors(bh, 10) ≈ 0.49104425779640487
+            @test disk_nt_rel_factors(bh, 50) ≈ 0.7072886586539658
+            @test disk_nt_rel_factors(bh, 200) ≈ 0.8339124564014411
+        end
+        @testset "Test disc radiated power" begin
+            bh = BlackHole(1e8 * M_SUN, 0.5, 0)
+            @test disk_flux(bh, 10) ≈ 6839115834162428.0 rtol = 1e-6
+            @test disk_flux(bh, 100) ≈ 3.924336168219069e13 rtol = 1e-6
+            bh = BlackHole(1e9 * M_SUN, 0.5, 0.9)
+            @test disk_flux(bh, 10) ≈ 889138044477300.2 rtol = 1e-6
+            @test disk_flux(bh, 100) ≈ 1662600341587.0435 rtol = 1e-6
+        end
+        @testset "UV fraction" begin
+            bh = BlackHole(1e8 * M_SUN, 0.5, 0)
+            uvf1 = uv_fraction(bh, 6.01)
+            uvf2 = uv_fraction(bh, 10)
+            uvf3 = uv_fraction(bh, 100)
+            @test uvf1 < uvf2
+            @test uvf3 < uvf2
+        end
     end
-
-    @testset "Disk UV fractions" begin
-    ckend
 end
-
