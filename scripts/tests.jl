c08f9102aa6cc864e441c2648f06b76dbe6ab2b0
Arnau Quera-Bofarull <arnauq@protonmail.com>
2021-03-25 22:46:12 +0000

    Went back to primitive wind hull

--- scripts/tests.jl
+++ scripts/tests.jl
@@ -5,88 +5,50 @@ using YAML, Profile, PProf, PyCall, ProgressMeter, PyPlot, JLD2
 using Qwind
 include("scripts/plotting.jl")
 
-@load "./scratch/trajectories_big.jld2"
-
-Qwind.get_intersection_times(integrators);
-
-Profile.clear()
-@profile Qwind.get_intersection_times(integrators);
-
-pprof()
-
-
-
-
-rin = 46.100482541805334
-#rin = 20.0
 config_path = "configs/config_test.yaml"
 config = YAML.load_file(config_path, dicttype = Dict{Symbol,Any})
-config[:initial_conditions][:r_in] = rin
 try
     mv(config[:integrator][:save_path], "backup", force = true)
 catch
 end
 model = Model(config);
 iterations_dict = Dict();
-iterations_dict[1] = Dict();
 run!(model, iterations_dict, parallel = true)
 
+QwindPlotting.plot_wind_hull(
+    iterations_dict[2]["radiative_transfer"].interpolator.wind_hull,
+    nr = 500,
+    nz = 500,
+    zmax=20
+)
+
 integrators = iterations_dict[1]["integrators"];
-
 intersection_times = get_intersection_times(integrators);
+integs_interpol = interpolate_integrators(
+    integrators,
+    max_times = intersection_times,
+    n_timesteps = 100,
+    log = true,
+);
+r, z, _, _, _ = reduce_integrators(integs_interpol);
+r0 = [integ.p.r0 for integ in integrators];
+windh = Hull(r, z, r0, sigdigits = 6);
 
-lr, lw = Qwind.compute_lines_range(model);
-fig, ax = plt.subplots()
-for ll in lr
-    ax.axvline(ll)
-end
-
-
-using JLD2
-
-@load "./scratch/trajectories.jld2" integrators
-#@load "./scratch/max_times.jld2" max_times
-
-max_times = get_intersection_times(integrators)
-
-
-integrators_interp = interpolate_integrators(integrators, max_times=max_times, n_timesteps=100, log=false);
-
-using ConcaveHull
 
 fig, ax = plt.subplots()
-for integ in integrators_interp
-    ax.plot(integ.r, integ.z)
-end
-
-fig,ax = plt.subplots()
-for integ in integrators 
-    ax.plot(integ.p.data[:r], integ.p.data[:z])
-end
-
-points = Hull(integrators, max_times, hull_sigdigits=6);
-
-fig,ax = plt.subplots()
-ps = reduce(hcat, points)
-ax.scatter(ps[1,:], ps[2,:])
-hull = ConcaveHull.concave_hull(points)
-#ax.scatter(hr, hz)
-ax.set_xlim(0,1000)
-ax.set_ylim(0,1000)
-
-#hull = Hull(integrators, max_times)
-
-fig, ax = plt.subplots()
-QwindPlotting.plot_wind_hull(hull, fig=fig, ax=ax, rmin=40, rmax=5000, zmax=5000, nr=500, nz=500)
-#ps = reduce(hcat, points)
-#ax.scatter(ps[1,:], ps[2,:], s=10, alpha=0.5)
-#vs = reduce(hcat, hull.vertices)
-#ax.scatter(vs[1,:], vs[2,:], color="red", alpha=0.5)
-for integ in integrators_interp
-    ax.plot(integ.r, integ.z)
-end
-ax.set_xlim(0, 5000)
-ax.set_ylim(0, 5000)
-
-
-
+#QwindPlotting.plot_streamlines(integrators, fig=fig, ax=ax)
+#for integ in integs_interpol
+#    ax.plot(integ.r, integ.z, "o-", color = "black")
+#end
+#ps = reduce(hcat, points);
+#ax.scatter(ps[1, :], ps[2, :])
+QwindPlotting.plot_wind_hull(
+    windh,
+    zmax = 50,
+    nr = 500,
+    nz = 500,
+    fig = fig,
+    ax = ax,
+)
+ax.set_xlim(0, 500)
+ax.set_ylim(0, 50)
--- src/radiative_transfer/wind_interpolator/wind_hull.jl
+++ src/radiative_transfer/wind_interpolator/wind_hull.jl
@@ -8,128 +8,36 @@ function filter_array(a, b, value)
 end
 
 function Hull(r::Vector{Float64}, z::Vector{Float64}, r0::Vector{Float64}; sigdigits=6)
-    # this stores the maximum heights of each streamline, roughly
-    z_max = zeros(length(r0))
-    for (rp, zp) in zip(r, z)
-        if rp > r0[end] + 1
-            continue
-        end
-        ridx = searchsorted_nearest(r0, rp)
-        z_max[ridx] = max(z_max[ridx], zp)
-    end
-    ridx = searchsorted_nearest(r0, r[end])
-    r0 = r0[z_max .> 0]
-    z_max = z_max[z_max .> 0]
-
-    # then get points in between trajectories, this avoids
-    # the hull being too tight between trajectories
-    r_range = range(minimum(r0), maximum(r0), step=1)
-    z_range = zeros(length(r_range))
-    for (i, rp) in enumerate(r_range)
-        ridx = searchsorted_nearest(r0, rp)
-        z_range[i] = max(z_range[i], z_max[ridx])
-    end
-    r = vcat(r, r_range)
-    z = vcat(z, z_range)
-
     # remove points that are too close to each other
     points = hcat(r, z)
     points = round.(points, sigdigits = sigdigits)
     points = unique(points, dims = 1)
     points = [[points[i, 1], points[i, 2]] for i = 1:size(points)[1]]
-    @info "Constructing wind hull..."
-    flush()
-    hull = ConcaveHull.concave_hull(points)
-    if !hull.converged
-        error("Hull did not converge!")
-    end
-    @info "Done"
-    return hull #, points
+    hull = ConcaveHull.concave_hull(points, 1)
+    return hull#, points
 end
 
-#function Hull(r::Vector{Float64}, z::Vector{Float64}, r0::Vector{Float64}; sigdigits=6)
-#    hull_r_range = Float64[]
-#    hull_z_range = Float64[]
-#    # this stores the maximum heights of each streamline, roughly
-#    z_max = zeros(length(r0))
-#    for (rp, zp) in zip(r, z)
-#        if rp > r0[end] + 1
-#            continue
-#        end
-#        ridx = searchsorted_nearest(r0, rp)
-#        z_max[ridx] = max(z_max[ridx], zp)
-#    end
-#    r0 = r0[z_max .> 0]
-#    z_max = z_max[z_max .> 0]
-#    hull_r_range = vcat(hull_r_range, r0)
-#    hull_z_range = vcat(hull_z_range, z_max)
-#
-#    # we also include the (r0, 0) line
-#    hull_r_range = vcat(hull_r_range, r0)
-#    hull_z_range = vcat(hull_z_range, zeros(length(r0)))
-#
-#    ## Now we get the general shell of the wind
-#    r_range = range(minimum(r), maximum(r), step=1)
-#    z_range = range(minimum(z), maximum(z), step=1)
-#    zmax = zeros(length(r_range))
-#    zmin = zeros(length(r_range))
-#    rmin = Inf .* ones(length(z_range))
-#    rmax = zeros(length(z_range))
-#
-#    for (rp, zp) in zip(r, z)
-#        ridx = searchsorted_nearest(r_range, rp)
-#        zmax[ridx] = max(zmax[ridx], zp)
-#        zidx = searchsorted_nearest(z_range, zp)
-#        rmax[zidx] = max(rmax[zidx], rp)
-#        rmin[zidx] = min(rmin[zidx], rp)
-#    end
-#
-#    # add max z
-#    z_filter, r_filter = filter_array(zmax, r_range, 0.0)
-#    hull_r_range = vcat(hull_r_range, r_filter)
-#    hull_z_range = vcat(hull_z_range, z_filter)
-#
-#    # add min z
-#    z_filter, r_filter = filter_array(zmin, r_range, Inf)
-#    hull_r_range = vcat(hull_r_range, r_filter)
-#    hull_z_range = vcat(hull_z_range, z_filter)
-#
-#    # add max r
-#    r_filter, z_filter = filter_array(rmax, z_range, 0.0)
-#    hull_r_range = vcat(hull_r_range, r_filter)
-#    hull_z_range = vcat(hull_z_range, z_filter)
-#
-#    # add min r
-#    r_filter, z_filter = filter_array(rmin, z_range, Inf)
-#    hull_r_range = vcat(hull_r_range, r_filter)
-#    hull_z_range = vcat(hull_z_range, z_filter)
-#
-#    # remove points that are too close to each other
-#    points = hcat(hull_r_range, hull_z_range)
-#    points = round.(points, sigdigits = sigdigits)
-#    points = unique(points, dims = 1)
-#    points = [[points[i, 1], points[i, 2]] for i = 1:size(points)[1]]
-#    return points
-#    @info "Constructing wind hull with $(length(points)) points."
-#    flush()
-#    hull = ConcaveHull.concave_hull(points)
-#    if !hull.converged
-#        error("Hull did not converge!")
-#    end
-#    @info "Done"
-#    return hull#, points
-#end
-
-function Hull(integrators::Vector{<:Sundials.IDAIntegrator}, max_times; hull_sigdigits=6)
+function Hull(integrators::Vector{<:Sundials.IDAIntegrator}, max_times)
     r0 = [integ.p.r0 for integ in integrators]
     integrators_interpolated_linear = interpolate_integrators(
         integrators,
         max_times = max_times,
         n_timesteps = 100,
-        log = false,
+        log = true,
     )
     r, z, _, _, _ = reduce_integrators(integrators_interpolated_linear)
-    hull = Hull(r, z, r0, sigdigits=hull_sigdigits)
+    @info "Constructing wind hull"
+    hull = nothing
+    for sigd in [6, 5, 4]
+        hull = Hull(r, z, r0, sigdigits=sigd)
+        if !hull.converged
+            @warn "Hull did not converge, trying with less sigdigits..."
+        end
+    end
+    if !hull.converged
+        error("Hull did not converge!")
+    end
+    @info "Done"
     return hull
 end
 
