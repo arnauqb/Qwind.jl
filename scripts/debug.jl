00f227fef19e5dcc29544ab173e5dbd9eebcb4fe
Arnau Quera-Bofarull <arnauq@protonmail.com>
2021-08-18 17:50:15 +0100

    adapting debug

--- scripts/debug.jl
+++ scripts/debug.jl
@@ -4,7 +4,7 @@ using Distributed
 @everywhere using Qwind
 using YAML, HDF5, CSV, DataFrames, PyPlot
 include("scripts/plotting.jl")
-using Profile, PProf
+using Profile, PProf, TimerOutputs, BenchmarkTools
 
 function get_model(config)
     model = Model(config)
@@ -19,93 +19,30 @@ end
 
 model, iterations_dict = get_model("./configs/debug.yaml");
 
-run!(model, iterations_dict);
-
 run_iteration!(model, iterations_dict, it_num = 1, parallel = true);
 
-run_iteration!(model, iterations_dict, it_num = 2);
 
-xl = model.rad.xray_luminosity;
-Rg = model.bh.Rg;
-r_range = range(0, 1000.0, length = 500);
-z_range = range(0, 1000.0, length = 500);
-r_range_test = range(0.0, 1000, length = 100);
-z_range_test = range(0.0, 1000, length = 100);
-density_grid = 2e8 .* ones((length(r_range), length(z_range)));
-grid = DensityGrid(r_range, z_range, density_grid);
-res = zeros((length(r_range_test), length(z_range_test)));
-for (i, r) in enumerate(r_range_test)
-    for (j, z) in enumerate(z_range_test)
-        res[i, j] = compute_xray_tau(grid, Boost(), 0.0, 0.0, r, z, xl, Rg)
-    end
-end
-
-LogNorm = matplotlib.colors.LogNorm;
-
-f, ax = plt.subplots()
-#cm = ax.pcolormesh(r_range_test, z_range_test, res', norm=LogNorm())
-cm = ax.contourf(r_range_test, z_range_test, res', norm = LogNorm())
-plt.colorbar(cm, ax = ax)
-
-
-run_iteration!(model, iterations_dict, it_num = 1, parallel = true)
-
-run_iteration!(model, iterations_dict, it_num = 2, parallel = true)
-
-#using JLD2
-#@save "./it_dict.jld2" iterations_dict
-
-density_grid = iterations_dict[2]["radiative_transfer"].interpolator.density_grid;
-rt = iterations_dict[2]["radiative_transfer"];
-
-
-
-fig, ax = plt.subplots()
-integs1 = iterations_dict[1]["integrators"];
-integs2 = iterations_dict2[1]["integrators"];
-for integ in integs1
-    #ax.plot(integ.p.data[:r], integ.p.data[:z], color = "C0")
-end
-for integ in integs2
-    ax.plot(integ.p.data[:r], integ.p.data[:z], color = "C1")
-end
-
-function profile_tauuv(grid, iterator, Rg)
-    #r_range = 10 .^ range(-3, 2, length=25)
-    #z_range = 10 .^ range(-3, 2, length=25)
-    #for rp in r_range
-    #    for zp in z_range
-    #        #compute_disc_radiation_field(rt, rp, zp, 0.0, 0.0)
-    #        compute_disc_radiation_field(rt, rp, zp, 0.0, 0.0)
-    #    end
-    #end
-    rp_range_test = range(6.0, 1500.0, length = 20)
-    r_range_test = range(6.0, 1500.0, length = 20)
-    z_range_test = 10 .^ range(-6, 3.0, length = 20)
-    phid_range_test = range(0.0, π, length = 20)
-    for rdp in rp_range_test
-        for rp in r_range_test
-            for zp in z_range_test
-                for pd in phid_range_test
-                    qwsol = compute_uv_tau(grid, iterator, rdp, pd, 0.0, rp, 0.0, zp, Rg)
-                end
-            end
+function profile_integ(model)
+    rr = range(20.0, 1000.0, length=10)
+    zz = 10 .^ range(-6, 3.0, length=10)
+    for r in rr
+        for z in zz
+            compute_disc_radiation_field(model, r, z, 0.0, 0.0)
         end
     end
 end
 
+disable_timer!(timer)
+            
+enable_timer!(timer)
+
+reset_timer!(timer); compute_disc_radiation_field(model.rad, r=100.0, z=1.0, vr=0.0, vz=0.0); timer
+
+
+@btime compute_disc_radiation_field(model.rad, r=100.0, z=1.0, vr=0.0, vz=0.0)
+
+profile_integ(model)
+
 Profile.clear()
-@profile profile_tauuv(density_grid, density_grid.iterator, model.bh.Rg)
+@profile profile_integ(model.rad)
 pprof()
-
-#run!(model, iterations_dict, parallel=true)
-Profile.clear()
-@profile run_iteration!(model, iterations_dict, it_num = 2, parallel = false)
-pprof()
-
-lr, lw = Qwind.compute_lines_range(model);
-
-fig, ax = plt.subplots()
-for r in lr
-    ax.axvline(r)
-end
