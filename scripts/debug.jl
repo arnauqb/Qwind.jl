50fd9249116c654e6998b18a957f2e6044c5c185
Arnau Quera-Bofarull <arnauq@protonmail.com>
2021-08-18 17:45:38 +0100

    debugging master

--- scripts/debug.jl
+++ scripts/debug.jl
@@ -2,11 +2,50 @@ using Distributed
 @everywhere using DrWatson
 @everywhere @quickactivate "Qwind"
 @everywhere using Qwind
-using YAML, HDF5, CSV, DataFrames
+using YAML, HDF5, CSV, DataFrames, PyPlot
 include("scripts/plotting.jl")
-matplotlib.rcParams["figure.dpi"] = 300
+using Profile, PProf, TimerOutputs, BenchmarkTools
 
-mv(model.config[:integrator][:save_path], "backup",  force=true)
-model = Model("results/resolution_tests/config1.yaml");
-iterations_dict = Dict();
-run!(model, iterations_dict)
+function get_model(config)
+    model = Model(config)
+    try
+        mv(model.config[:integrator][:save_path], "backup", force = true)
+    catch
+    end
+    model = Model(config)
+    iterations_dict = Dict()
+    return model, iterations_dict
+end
+
+model, iterations_dict = get_model("./configs/debug.yaml");
+
+run_iteration!(model, iterations_dict, it_num = 1, parallel = true);
+
+
+function profile_integ(model)
+    rr = range(20.0, 1000.0, length=10)
+    zz = 10 .^ range(-6, 3.0, length=10)
+    for r in rr
+        for z in zz
+            #compute_disc_radiation_field(radiation, r=r, z=z, vr=0.0, vz=0.0)
+            compute_disc_radiation_field(model.rt, r, z, 0.0, 0.0)
+        end
+    end
+end
+
+disable_timer!(timer)
+            
+enable_timer!(timer)
+
+reset_timer!(timer); compute_disc_radiation_field(model.rad, r=100.0, z=1.0, vr=0.0, vz=0.0); timer
+
+
+@btime compute_disc_radiation_field(model.rad, r=100.0, z=1.0, vr=0.0, vz=0.0)
+
+profile_integ(model)
+
+@btime profile_integ(model)
+
+Profile.clear()
+@profile profile_integ(model.rad)
+pprof()
