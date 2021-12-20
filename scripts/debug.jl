afdbc97a6d730b28a0159c0700df867ab3cdc62f
Arnau Quera-Bofarull <arnauq@protonmail.com>
2021-12-20 20:35:01 +0000

    debugging...

--- scripts/debug.jl
+++ scripts/debug.jl
@@ -3,11 +3,12 @@ using Distributed
 @everywhere @quickactivate "Qwind"
 @everywhere using Qwind
 
-#using YAML, HDF5, CSV, DataFrames, PyPlot
-#include("scripts/plotting.jl")
+using PyPlot
+#using YAML, HDF5, CSV, DataFrames
+include("scripts/plotting.jl")
 #using Profile, PProf, TimerOutputs, BenchmarkTools, ProgressMeter
-#LogNorm = matplotlib.colors.LogNorm
-#Normalize = matplotlib.colors.Normalize
+LogNorm = matplotlib.colors.LogNorm
+Normalize = matplotlib.colors.Normalize
 
 function get_model(config)
     model = Model(config)
@@ -20,5 +21,12 @@ function get_model(config)
     return model, iterations_dict
 end
 model, iterations_dict = get_model("./configs/proga.yaml");
+
+Qwind.CAK_Σ(model.wind.density_grid, model.wind.grid_iterator, model.parameters, model.rad, 100, K=0.03)
+
+fig, ax = plt.subplots()
+r_range = 10 .^ range(log10(6.1), log10(1500), length=50);
+n0 = getn0.(Ref(model), r_range);
+ax.loglog(r_range, n0, "o-")
 
 run!(model, iterations_dict, parallel = true)
